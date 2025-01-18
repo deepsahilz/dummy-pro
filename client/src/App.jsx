43a5e40a27ecc3383ae58798dae55de3cfd45879
deepsahilz <deepsahil.online@gmail.com>
2025-01-18 14:10:53 +0530

    ko

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 
   useEffect(() => {
     // Fetch todos from backend (dummy fetch for now)
-    fetch('https://todopankaj.vercel.app/todos')
+    fetch('https://todopankaj-server.vercel.app/todos')
       .then((res) => res.json())
       .then((data) => setTodos(data))
       .catch((err) => console.error(err));
